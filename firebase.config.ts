96e99a758da72d17c271a2ff1c130fdee4e596b3
221B0825 <eunseoyu0825@gmail.com>
2024-05-02 19:29:43 +0900

    feat: basic structure

---
+++ firebase.config.ts
@@ -0,0 +1,26 @@
+import { initializeApp } from 'firebase/app';
+import {
+  getAuth,
+  initializeAuth,
+  getReactNativePersistence,
+} from 'firebase/auth';
+import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage';
+
+//Please change it in your firebase.config.ts file.
+const firebaseConfig = {
+  apiKey: 'YOUR_API_KEY',
+  authDomain: 'YOUR_AUTH_DOMAIN',
+  projectId: 'YOUR_PROJECT_ID',
+  storageBucket: 'YOUR_STORAGE_BUCKET',
+  messagingSenderId: 'YOUR_MESSAGING_SENDER_ID',
+  appId: 'YOUR_APP_ID',
+  //measurementId can be omitted.
+  measurementId: 'YOUR_MEASUREMENT_ID',
+};
+
+export const FIREBASE_APP = initializeApp(firebaseConfig);
+initializeAuth(FIREBASE_APP, {
+  persistence: getReactNativePersistence(ReactNativeAsyncStorage),
+});
+
+export const FIREBASE_AUTH = getAuth(FIREBASE_APP);
